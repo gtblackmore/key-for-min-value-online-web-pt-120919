e1584fb914d6f63cd4652011acd369fd1a9aab05
George Blackmore <gtblackmore@gmail.com>
2019-12-16 22:24:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,12 @@ def key_for_min_value(name_hash)
   if name_hash.length < 1
     return nil
   end
-    if name_hash.each do |name, value|
-      val == -1 || value < val
-      val = value
-      key = name 
+  val = -1
+  key = " "
+  if name_hash.each do |name, value|
+    val == -1 || value < val
+    val = value
+    key = name 
     end 
   end
   key
